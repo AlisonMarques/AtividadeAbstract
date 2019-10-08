f1fa039f108c03a1530f146791efef1a2427ac94
Marcos Olavo <marcos.after@gmail.com>
2019-10-08 14:25:43 -0300

    melhorando a logica nas condição casal e adicionando toString para apresentar todos os dados das propriedades filhas e pai

--- Exercicio03/src/quest01/Cama.java
+++ Exercicio03/src/quest01/Cama.java
@@ -4,7 +4,6 @@ public class Cama extends Madeira {
 
 	private boolean casal;
 
-
 	public boolean isCasal() {
 		return casal;
 	}
@@ -14,20 +13,24 @@ public class Cama extends Madeira {
 	}
 
 	@Override
-	public String toString() {
-		return "[ Cama:" + this.isCasal()  + "]";
+	public void aumentarPreco(float percentual) {
+
+		if (casal) {
+
+			percentual = 1.5f;
+			System.out.println("O preÃ§o da cama de casal Ã© de: R$" + this.getPrecoVenda() * percentual);
+
+		} else {
+
+			percentual = 1.0f;
+			System.out.println("O preÃ§o da cama de solteiro Ã© de: R$ " + this.getPrecoVenda() * percentual);
+		}
 	}
 
 	@Override
-	public void aumentarPreco(float percentual) {
-		if(casal == true) {
-			percentual = 1.5f;
-			System.out.println("O preço da cama de casal é de: R$" + this.getPrecoVenda() * percentual);
-		}
-		if(casal == false) {
-			percentual = 1.0f;
-			System.out.println("O preço da cama de solteiro é de: R$ " + this.getPrecoVenda() * percentual);
-		}
+	public String toString() {
+		return "Cama [casal=" + casal + ", getTipoMadeira()=" + getTipoMadeira() + ", getMaterial()=" + getMaterial()
+				+ ", getDescricao()=" + getDescricao() + ", getCusto()=" + getCusto() + ", getPrecoVenda()="
+				+ getPrecoVenda() + "]";
 	}
-
 }
