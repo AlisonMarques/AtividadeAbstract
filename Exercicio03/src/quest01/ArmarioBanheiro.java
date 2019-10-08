b02dc6c70a7fb9d3c0690a107c207cbc8d4f57f0
Marcos Olavo <marcos.after@gmail.com>
2019-10-08 14:24:06 -0300

    melhorando a logica nas condições comVidro e adicionando toString para apresentar todos os dados das propriedades filhas e pai

--- Exercicio03/src/quest01/ArmarioBanheiro.java
+++ Exercicio03/src/quest01/ArmarioBanheiro.java
@@ -1,9 +1,8 @@
 package quest01;
 
-public class ArmarioBanheiro extends Plastico{
+public class ArmarioBanheiro extends Plastico {
 
 	private boolean comVidro;
-
 
 	public boolean isComVidro() {
 		return comVidro;
@@ -14,20 +13,24 @@ public class ArmarioBanheiro extends Plastico{
 	}
 
 	@Override
-	public String toString() {
-		return "Armario/Banheiro [" + "Com vidro:" + this.isComVidro();
+	public void aumentarPreco(float percentual) {
+
+		if (comVidro) {
+
+			percentual = 2.5f;
+			System.out.println("O preÃ§o com vidro Ã© de: R$ " + this.getPrecoVenda() * percentual);
+
+		} else {
+
+			percentual = 1.5f;
+			System.out.println("O preÃ§o sem vidro Ã© de: R$ " + this.getPrecoVenda() * percentual);
+		}
 	}
 
 	@Override
-	public void aumentarPreco(float percentual) {
-		if(comVidro = true) {
-			percentual = 2.5f;
-			System.out.println("O preço com vidro é de: R$ " + this.getPrecoVenda() * percentual);
-		}
-		if(comVidro = false) {
-			percentual= 1.5f;
-			System.out.println("O preço sem vidro é de: R$ " + this.getPrecoVenda() * percentual);
-		}
+	public String toString() {
+		return "ArmarioBanheiro [comVidro=" + comVidro + ", getQualidade()=" + getQualidade() + ", getMaterial()="
+				+ getMaterial() + ", getDescricao()=" + getDescricao() + ", getCusto()=" + getCusto()
+				+ ", getPrecoVenda()=" + getPrecoVenda() + "]";
 	}
-
 }
