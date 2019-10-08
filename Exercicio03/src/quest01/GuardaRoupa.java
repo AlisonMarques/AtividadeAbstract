16e7cb1aebbd926ffc3a2ba54375a5f19f6344d4
Marcos Olavo <marcos.after@gmail.com>
2019-10-08 14:26:56 -0300

    adicionando toString para apresentar todos os dados das propriedades filhas e pai

--- Exercicio03/src/quest01/GuardaRoupa.java
+++ Exercicio03/src/quest01/GuardaRoupa.java
@@ -5,39 +5,42 @@ public class GuardaRoupa extends Madeira {
 	private int numPortas;
 	private float largura;
 	private float altura;
-	
+
 	public int getNumPortas() {
 		return this.numPortas;
 	}
+
 	public void setNumPortas(int numPortas) {
 		this.numPortas = numPortas;
 	}
+
 	public float getLargura() {
 		return this.largura;
 	}
+
 	public void setLargura(float largura) {
 		this.largura = largura;
 	}
+
 	public float getAltura() {
 		return this.altura;
 	}
+
 	public void setAltura(float altura) {
 		this.altura = altura;
 	}
-	
-	@Override
-	public String toString() {
-		return "Guarda Roupa [" + "Número de portas:" + numPortas +
-				"Largura:" + largura +
-				"Altura:" + altura + "]";
-	}
-	
+
 	@Override
 	public void aumentarPreco(float percentual) {
 		percentual = 1.5f;
 		System.out.println("O valor aumentou para: R$ " + this.getPrecoVenda() * percentual);
-		
+
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "GuardaRoupa [numPortas=" + numPortas + ", largura=" + largura + ", altura=" + altura
+				+ ", getTipoMadeira()=" + getTipoMadeira() + ", getMaterial()=" + getMaterial() + ", getDescricao()="
+				+ getDescricao() + ", getCusto()=" + getCusto() + ", getPrecoVenda()=" + getPrecoVenda() + "]";
+	}
 }
